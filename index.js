6eccaf93027a6b94fe7e378c98cc75a6bfe67ac7
keenanhumm <keenan.humm@huskers.unl.edu>
2018-06-15 16:35:37 -0500

    ssl

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('./server/server');
 const port = process.env.PORT || 4000;
 const key =
-  "qvtnHGoIvd6mo6yr1izcQURTBq0cnbWAF3DsJLUANRA.8EJyNKGsT3S5SsC5oJtcqm-N7O6_-lVrM9UnfNYm8zU";
-
-app.get(`/.well-known/acme-challenge/${key}`, function (req, res) {
+  "xsgMbsKGfvziD3z4GkeNYuQ4-3sWuPLDY2JnN9OPaA0.8EJyNKGsT3S5SsC5oJtcqm-N7O6_-lVrM9UnfNYm8zU";
+const addressSSL = "xsgMbsKGfvziD3z4GkeNYuQ4-3sWuPLDY2JnN9OPaA0";
+app.get(`/.well-known/acme-challenge/${addressSSL}`, function (req, res) {
   res.send(key);
 })
 
